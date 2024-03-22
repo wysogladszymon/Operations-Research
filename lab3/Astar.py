38fd06c9008cf8da9bc5b70ff556cb5e16a3d343
Szymon Wysogląd <wysogladszymon@gmail.com>
2024-03-22 08:55:58 +0100

    start of astar

---
+++ lab3/Astar.py
@@ -0,0 +1,12 @@
+import math
+import pygame
+
+
+def main():
+  WIDTH = 800
+  WIN = pygame.display.set_mode((WIDTH, WIDTH))
+  pygame.display.set_caption("A* Pathfinding algorithm")
+
+
+if __name__ == '__main__':
+  main()
